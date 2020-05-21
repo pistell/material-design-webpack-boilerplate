ae8765b7944c47270f0c4140cf69a4bf8ab2dd10
James Pistell <jamespistell@gmail.com>
2020-05-21 04:33:24 -0400

    Added notes for images and webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,11 @@ import '../styles/index.scss';
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
 
+// In order to add image files to your page via Webpack you need to import them first
+import webpackLogo from '../img/icon-square-big.png';
+import materialDesignLogo from '../img/material-design-logo.png';
+import javaScriptLogo from '../img/JavaScript-logo.png';
+
 // Instantiation of Material Design Components
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const buttons = document.querySelectorAll('.mdc-button');
@@ -22,3 +27,21 @@ buttons.forEach((btn) => {
 const topAppBarHeight = topAppBar.root_.getBoundingClientRect().height;
 const main = document.querySelector('main');
 main.style.paddingTop = `${topAppBarHeight + 10}px`;
+
+// An example of how to add images to the page from Webpack
+// Add the image files to an array so we can iterate over them
+const logoArray = [webpackLogo, materialDesignLogo, javaScriptLogo];
+const logoDivArray = document.querySelectorAll('div.imageDiv');
+logoArray.forEach((logo, index) => {
+  // Create a new image --> "new Image(width, height);"
+  const logoImage = new Image(200, 200);
+  // Add the src data to the new image
+  logoImage.src = logo;
+  // Optional - Adjust the styles for the parent element of the image
+  logoDivArray[index].parentElement.style.justifyContent = 'center';
+  logoDivArray[index].parentElement.style.alignItems = 'center';
+  logoDivArray[index].parentElement.style.paddingBottom = '10px';
+  logoDivArray[index].parentElement.classList.add('mdc-elevation--z4');
+  // Append the image to the div
+  logoDivArray[index].appendChild(logoImage);
+});
